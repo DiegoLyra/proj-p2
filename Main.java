7ad7f0af21c93f6b9119eba06416396ba16beda4
Diego Lyra <diegofernandes.lyra@gmail.com>
2026-05-05 17:17:53 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -51,3 +51,52 @@ public class Main {
         sc.close();
     }
 }
+
+// ideia para o menu
+
+import service.analyzer;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        boolean saidaUser = false;
+
+        while (!saidaUser) {
+            exibirMenu();
+            String entrada = scanner.nextLine();
+
+            if (entrada.equals("1")) {
+                analyzer.analisarRecursosGrandes();
+
+            } else if (entrada.equals("2")) {
+                analyzer.analisarNaoRespondidas();
+
+            } else if (entrada.equals("3")) {
+                analyzer.analisarSistemasOperacionais();
+
+            } else if (entrada.equals("4")) {
+                analyzer.analisarMediaPost();
+
+            } else if (entrada.equals("0")) {
+                System.out.println("Encerrando o programa...");
+                saidaUser = true;
+
+            } else {
+                System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+
+    private static void exibirMenu() {
+        System.out.println("\n--- DASHBOARD DE REQUISIÇÕES ---");
+        System.out.println("1 - Recursos Grandes Respondidos");
+        System.out.println("2 - Não Respondidos");
+        System.out.println("3 - % de Requisições por SO");
+        System.out.println("4 - Média das Requisições POST");
+        System.out.println("0 - Sair\n");
+    }
+}
