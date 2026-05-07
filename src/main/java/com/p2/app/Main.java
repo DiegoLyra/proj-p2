c75c9d13acd626f07480573e79674e051b324cc5
Diego Lyra <diegofernandes.lyra@gmail.com>
2026-05-06 22:06:15 -0300

    implementando carregamento do arquivo de log

--- src/main/java/com/p2/app/Main.java
+++ src/main/java/com/p2/app/Main.java
@@ -1,9 +1,16 @@
 package br.upe.app;
 
+import br.upe.data.LogEntry;
+import br.upe.parser.LogParser;
+
+import java.util.List;
+
 public class Main {
     private static final String CAMINHO_LOG = "access.log";
 
     public static void main(String[] args){
-        
+        System.out.println("Carregando arquivo de log...");
+        List<LogEntry> entradas = LogParser.loadFile(CAMINHO_LOG);
+        System.out.println(entradas.size() + " registros carregados.")
     }
 }
