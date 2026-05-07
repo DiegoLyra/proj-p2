d0cb6a127464a5a264ddabb9f16a3f1665b83e2d
Diego Lyra <diegofernandes.lyra@gmail.com>
2026-05-06 21:38:44 -0300

    criando estrutura inicial do menu

--- src/main/java/com/p2/app/Menu.java
+++ src/main/java/com/p2/app/Menu.java
@@ -1,6 +1,37 @@
-package com.p2;
+package com.p2.app;
 
-public class AnaliseLog {
-    
+import br.upe.data.LogEntry;
 
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+    private List<LogEntry> entradas;
+    private Scanner scanner;
+
+    public Menu(List<LogEntry> etradas){
+        this.entradas = entradas;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        int opcao = -1;
+
+        while (opcao != 0){
+            System.out.println("\n1 - Recursos grandes respondidos");
+            System.out.println("2 - Nao respondidos");
+            System.out.println("3 - % de requisicoes por SO");
+            System.out.println("4 - Media das requisicoes por POST");
+            System.out.println("0 - Sair");
+            System.out.println("| ");
+
+            try{
+                opcao = Intereger.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                System.out.println("Opcao invalida.");
+                continue;
+            }
+        }
+        scanner.close();   
+    }
 }
